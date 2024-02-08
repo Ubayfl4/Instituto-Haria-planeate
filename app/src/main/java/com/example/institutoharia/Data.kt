084c6aae0231e4a99382ff62f7b41acc8b3b7adc
Ubayfl4 <114585628+Ubayfl4@users.noreply.github.com>
2024-02-08 10:25:28 +0000

    for y sheets

---
+++ app/src/main/java/com/example/institutoharia/Data.kt
@@ -0,0 +1,5 @@
+package com.example.institutoharia
+
+import androidx.compose.ui.graphics.painter.Painter
+
+class Data(val textoCard: String, val textoModal: String, val textoImagen: String, val imagen: Painter)
--- app/src/main/java/com/example/institutoharia/MainActivity.kt
+++ app/src/main/java/com/example/institutoharia/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.institutoharia
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -31,6 +33,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -53,6 +56,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.institutoharia.ui.theme.InstitutoHariaTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,6 +110,18 @@ fun MyApp(modifier: Modifier){
 @Composable
 fun Contenido() {
     var estadoSheets by rememberSaveable { mutableStateOf(false) }
+    var contenidoSheets by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    val listaCotenidoInicio = listOf(
+        Data(textoCard = "Cableado estructurado", textoModal = "Hola 1", textoImagen = "hola", imagen = painterResource(id = R.drawable.cable)),
+        Data(textoCard = "Diseño de páginas web", textoModal = "Hola 2", textoImagen = "hola", imagen = painterResource(id = R.drawable.codigohtml)),
+        Data(textoCard = "Elaboración de juegos", textoModal = "Hola 3", textoImagen = "hola", imagen = painterResource(id = R.drawable.juegofinal)),
+        Data(textoCard = "Montaje de equipos", textoModal = "Hola 4", textoImagen = "hola", imagen = painterResource(id = R.drawable.montajefinal)),
+        Data(textoCard = "Diseño de redes", textoModal = "Hola 5", textoImagen = "hola", imagen = painterResource(id = R.drawable.red)),
+        Data(textoCard = "Formación en centros de trabajo", textoModal = "Hola 6", textoImagen = "hola", imagen = painterResource(id = R.drawable.fol)),
+    )
 
     Column(modifier = Modifier
         .padding(top = 80.dp)
@@ -114,68 +130,43 @@ fun Contenido() {
            horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "PROYECTO/ACTIVIDADES", fontWeight = FontWeight.Bold, fontSize = 25.sp)
         Text(text = "Proyectos y actividades que se eleboran en estos cursos", fontSize = 17.sp, textAlign = TextAlign.Center)
-        Row(modifier = Modifier.padding(vertical = 10.dp)) {
-            ElevatedCard (modifier = Modifier
-                .clickable {/*TODO*/ }
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.cable), contentDescription = "hola")
-                Text(text = "Cableado estructurado",fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
-            }
-            ElevatedCard (modifier = Modifier
-                .clickable {/*TODO*/ }
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.codigohtml), contentDescription = "hola")
-                Text(text = "Diseño de páginas web", fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
-            }
-        }
-        Row(modifier = Modifier.padding(vertical = 10.dp)) {
-            ElevatedCard (modifier = Modifier
-                .clickable { /*TODO*/}
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.juegofinal), contentDescription = "hola")
-                Text(text = "Elaboración de juegos",fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
-            }
-            ElevatedCard (modifier = Modifier
-                .clickable {/*TODO*/ }
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.montajefinal), contentDescription = "hola")
-                Text(text = "Montaje de equipo", fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
+        for (index in listaCotenidoInicio.indices step 2) {
+            Row (modifier = Modifier.padding(vertical = 10.dp)) {
+                var maxIndex = index + 1
+                if (maxIndex >= listaCotenidoInicio.size) {
+                    maxIndex -= 1
+                }
+                for (currentIndex in index .. maxIndex) {
+                    val data = listaCotenidoInicio[currentIndex]
+                    ElevatedCard (modifier = Modifier
+                        .clickable {
+                            estadoSheets = true
+                            contenidoSheets = data.textoModal
+                        }
+                        .padding(10.dp)
+                        .weight(1f)) {
+                        Image(painter = data.imagen, contentDescription = data.textoImagen)
+                        Text(text = data.textoCard, fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
+                    }
+                }
             }
         }
-        Row(modifier = Modifier.padding(vertical = 10.dp)) {
-            ElevatedCard (modifier = Modifier
-                .clickable {/*TODO*/ }
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.red), contentDescription = "hola")
-                Text(text = "Diseño de red",fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
-            }
-            ElevatedCard (modifier = Modifier
-                .clickable { Sheets() }
-                .padding(10.dp)
-                .weight(1f)){
-                Image(painter = painterResource(id = R.drawable.fol), contentDescription = "hola")
-                Text(text = "Formación en centros de trabajo", fontWeight = FontWeight.Bold, modifier = Modifier.padding(10.dp))
-            }
-        }
+        Sheets({ estadoSheets = false; contenidoSheets = "" }, estadoSheets, contenidoSheets)
     }
-    Sheets()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Sheets(estadoSheets){
-
+fun Sheets(onDismissModalBottomSheet: () -> Unit, estadoSheets: Boolean, contenidoSheets: String) {
+    val sheetState = rememberModalBottomSheetState()
     if (estadoSheets){
         ModalBottomSheet(
-            onDismissRequest = { estadoSheets = false },
-
+            onDismissRequest = { onDismissModalBottomSheet() },
+            sheetState = sheetState
         ) {
-
+            Row(modifier = Modifier.padding(60.dp)) {
+                Text(text = contenidoSheets)
+            }
         }
     }
 }
